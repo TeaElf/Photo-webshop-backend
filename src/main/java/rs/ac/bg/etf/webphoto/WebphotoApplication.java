a4fee9832f5e71de3ecf47f59a1085b14be80221
tea <teodora.pekovic@netcast.rs>
2021-03-06 16:46:42 +0100

    Init repo - this commit is a lie

---
+++ src/main/java/rs/ac/bg/etf/webphoto/WebphotoApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.etf.webphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebphotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebphotoApplication.class, args);
+	}
+
+}
