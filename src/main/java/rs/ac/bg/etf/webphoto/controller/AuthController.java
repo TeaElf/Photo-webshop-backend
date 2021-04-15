e13adb40b96410c1ab105489c171febad0fa2f40
tea <teodora.pekovic@netcast.rs>
2021-04-15 11:52:30 +0200

    auth controller

---
+++ src/main/java/rs/ac/bg/etf/webphoto/controller/AuthController.java
@@ -0,0 +1,18 @@
+package rs.ac.bg.etf.webphoto.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    // TODO
+    // login
+    // sign up
+    // forgot password
+    // change password
+
+}
